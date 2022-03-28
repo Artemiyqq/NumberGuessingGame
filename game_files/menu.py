f0affeea21903e4966dd6e7b9a7fb14369b17151
Artemiyqq <artom.florko@gmail.com>
2022-03-28 21:36:04 +0300

    Work on menu continues

--- game_files/menu.py
+++ game_files/menu.py
@@ -1,24 +1,28 @@
-import random
-import general_functions
-import os
+import random, general_functions, os, sys
 
 
 class MenuClass:
     text = general_functions.parse_program_text(os.path.realpath('..\\localisation\\english_language.json'))
 
-    def launch_selected_action(self):
-        pass
-
     def choosing_action(self):
-        num_of_action = input(f"{self.text['greeting'], self.text['menu_text']")
+        self.num_of_action = input(f"{self.text['greeting']}{self.text['menu_text']}")
         while True:
-            if not num_of_action in '123':
-                num_of_action = input(random.choice(self.text['not_correct_value']))
+            if not self.num_of_action in '123':
+                self.num_of_action = input(random.choice(self.text['not_correct_value']))
             else:
-                return num_of_action
+                break
+
+    def launch_selected_action(self):
+        match self.num_of_action:
+            case '1':
+                pass
+            case '2':
+                pass
+            case '3': sys.exit(self.text['parting'])
 
     def main(self):
         self.choosing_action()
+        self.launch_selected_action()
 
 
 menu = MenuClass()
