f91ffd7b97f86c7cb3002e63bdac0339f6884aa0
Artemiyqq <artom.florko@gmail.com>
2022-05-14 19:16:33 +0300

    mostly ended work on Ukrainian localisation. Maybe will be needed to fix some bags in future

--- leaderboard.py
+++ leaderboard.py
@@ -5,12 +5,18 @@ from modules.input_parsing import ParseInput
 
 
 class LeaderboardClass:
+    mini_words = {'English': ['with', 'p.'], 'Ukrainian': ['з', 'о.']}
+
     def print_top10(self):
         print(self.text['leader_board_message'])
         top_players = Database.querying_db_data("SELECT * FROM leader_board ORDER BY userscore DESC")
         for some_of_top in enumerate(self.text['top10']):
             try:
-                print(f'{some_of_top[1]} {top_players[some_of_top[0]][0]} with {top_players[some_of_top[0]][1]} p.')
+                print(f'{some_of_top[1]} '
+                      f'{top_players[some_of_top[0]][0]} '
+                      f'{self.mini_words[self.current_language][0]} '
+                      f'{top_players[some_of_top[0]][1]} '
+                      f'{self.mini_words[self.current_language][1]}')
             except IndexError:
                 print(f'{some_of_top[1]} ...')
         sleep(4)
